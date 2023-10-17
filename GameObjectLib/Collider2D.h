47af63a270b68af8aca74dce1115b4c5a9ec6442
unknown <vlopez@gamingcampus.local>
2023-10-17 11:00:38 +0200

    Push victor

--- GameObjectLib/Collider2D.h
+++ GameObjectLib/Collider2D.h
@@ -1,17 +1,25 @@
 #pragma once
 #include "Vector2D.h"
-class Collider2D 
+class Collider2D
 {
-public :
+public:
 	Collider2D(Vector2D<float> position, float width, float height);
-	~Collider2D();
+	Collider2D(float x, float y, float width, float height) {
+		Vector2D<auto>(x, y) position;
+	}
 
 	float getX() { return position.getX; }
 	float getY() { return position.getY; }
+	Vector2D<auto> getVector2D() { return position; }
+
 	float getWidth() { return width; }
 	float getHeight() { return height; }
 
-protected :
+	bool collisionTrigger(const Collider2D& colliderA, const Collider2D& colliderB);
+
+	~Collider2D();
+
+protected:
 	Vector2D<float> position;
 	float width, height;
 };
